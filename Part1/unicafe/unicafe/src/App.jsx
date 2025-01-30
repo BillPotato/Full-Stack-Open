6250052e0c97a7ec82288cfa2867ae4391e2b957
Bill Nguyen <nbpntp@gmail.com>
2025-01-31 03:15:57 +0700

    1.11

--- Part1/unicafe/unicafe/src/App.jsx
+++ Part1/unicafe/unicafe/src/App.jsx
@@ -9,7 +9,10 @@ const Button = ({onClick, label}) => (
   )
 
 const StatisticLine = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
   )
 
 const Statistics = ({good, neutral, bad}) => {
@@ -26,14 +29,16 @@ const Statistics = ({good, neutral, bad}) => {
   
   // Yes feedback
   return (
-    <div>
+    <table>
+      <tbody>
         <StatisticLine text="good" value={good} />
         <StatisticLine text="neutral" value={neutral} />
         <StatisticLine text="bad" value={bad} />
         <StatisticLine text="total" value={total} />
         <StatisticLine text="average" value={average} />
         <StatisticLine text="positive" value={positive} />
-    </div>
+      </tbody>
+    </table>
     )
   }
 
