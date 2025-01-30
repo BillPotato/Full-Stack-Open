25c7a80f393dd7eba9aba3cf34f04d50398d040b
Bill Nguyen <nbpntp@gmail.com>
2025-01-31 02:46:45 +0700

    1.7

--- Part1/unicafe/unicafe/src/App.jsx
+++ Part1/unicafe/unicafe/src/App.jsx
@@ -8,13 +8,22 @@ const Button = ({onClick, label}) => (
   <button onClick={onClick}>{label}</button>
   )
 
-const Statistics = ({good, neutral, bad}) => (
-  <div>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-  )
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = `${(good/total)*100} %`
+  
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </div>
+    )
+  }
 
 const App = () => {
   // save clicks of each button to its own state
