25ac1f68d22cc6d29d6fa1e8629d81285f96a5c4
unknown <nbpntp@gmail.com>
2025-07-18 02:12:58 +0700

    3.12

--- Part3/phonebook_frontend/src/components/FilterField.jsx
+++
@@ -1,9 +0,0 @@
-const FilterField = ({filterWord, onFilterChange}) => {
-  return (
-    <div>
-      filter shown with <input value = {filterWord} onChange = {onFilterChange} />
-    </div>
-  )
-}
-
-export default FilterField
