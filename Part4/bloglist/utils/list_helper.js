749285b9f6c0693431af8c822544c46cebc3b771
unknown <nbpntp@gmail.com>
2025-07-25 05:04:29 +0700

    4.5

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
 	return blogs.reduce(reducer, 0)
 }
 
+const favouriteBlog = (blogs) => {
+	let Mi = -1
+
+	for (let i = 0; i < blogs.length; i++) {
+		// console.log(`i: ${i} - Mi: ${Mi}`)
+		Mi = (Mi == -1)
+			? i
+			: (blogs[Mi].likes < blogs[i].likes)
+				? i
+				: Mi
+	}
+
+	return blogs[Mi]
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favouriteBlog,
 }
