35becf3531f1677b9462a87d08ffdde1cf6d969a
Bill <nbpntp@gmail.com>
2026-01-28 19:48:26 -0500

    9.9

--- Part9/patientor-backend/index.ts
+++ Part9/patientor-backend/index.ts
@@ -1,12 +1,15 @@
 import express from "express"
+import cors from "cors";
 
 const app = express();
 
-app.get("/ping", (_req, res) => {
+app.use(cors());
+
+app.get("/api/ping", (_req, res) => {
     res.send("pong");
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 })
